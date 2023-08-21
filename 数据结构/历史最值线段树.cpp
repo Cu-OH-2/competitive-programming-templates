6bbe5e597bfaa81c787b2d34950607c9f67830e4
Cu_OH_2 <850250678@qq.com>
2023-08-22 04:40:32 +0800

    更新历史最值线段树

---
+++ 数据结构/历史最值线段树.cpp
@@ -0,0 +1,81 @@
+/*******************************************************************
+* 时间复杂度：询问O(logn)/修改O(logn)
+* 说明：
+* 1.维护区间历史最值，支持区间加减
+* 2.上方标记一定新于下方标记，因此下传可以整体施加
+*******************************************************************/
+struct SegTree
+{
+    struct Node
+    {
+        int lef, rig;
+        ll mval; //历史最值
+        ll tag, mtag; //当前修改标签、tag生命周期内最值
+    };
+    vector<Node> tree;
+    inline ll merge(ll x, ll y) { return min(x, y); } //最大还是最小
+    inline void affect(ll& x, ll y) { x = merge(x, y); } //取最值
+    inline void update(int src) //由子节点及其标记更新父节点
+    {
+        ll lv = tree[src << 1].mval + merge(tree[src << 1].mtag, 0);
+        ll rv = tree[src << 1 | 1].mval + merge(tree[src << 1 | 1].mtag, 0);
+        tree[src].mval = merge(lv, rv);
+        return;
+    }
+    inline void push(int src) //下传标记并消耗
+    {
+        if (tree[src].lef < tree[src].rig)
+        {
+            affect(tree[src << 1].mtag, tree[src << 1].tag + tree[src].mtag);
+            affect(tree[src << 1 | 1].mtag, tree[src << 1 | 1].tag + tree[src].mtag);
+            tree[src << 1].tag += tree[src].tag;
+            tree[src << 1 | 1].tag += tree[src].tag;
+        }
+        tree[src].mval += merge(tree[src].mtag, 0);
+        tree[src].mtag = tree[src].tag = 0;
+        return;
+    }
+    inline void mark(int src, ll val) //更新标记
+    {
+        tree[src].tag += val;
+        affect(tree[src].mtag, tree[src].tag);
+        return;
+    }
+
+    SegTree() {}
+    SegTree(int x) { init(x); }
+    void init(int x) { tree.resize(x * 4 + 1); }
+
+    void build(int src, int lef, int rig)
+    {
+        tree[src] = { lef, rig, 0, 0, 0 };
+        if (lef == rig) return;
+        int mid = lef + (rig - lef) / 2;
+        build(src << 1, lef, mid);
+        build(src << 1 | 1, mid + 1, rig);
+        update(src);
+        return;
+    }
+    void modify(int src, int lef, int rig, ll val)
+    {
+        if (lef <= tree[src].lef && tree[src].rig <= rig)
+        {
+            mark(src, val);
+            return;
+        }
+        push(src);
+        if (lef <= tree[src << 1].rig) modify(src << 1, lef, rig, val);
+        if (rig >= tree[src << 1 | 1].lef) modify(src << 1 | 1, lef, rig, val);
+        update(src);
+        return;
+    }
+    ll query(int src, int lef, int rig)
+    {
+        push(src);
+        if (lef <= tree[src].lef && tree[src].rig <= rig) return tree[src].mval;
+        ll res = 0;
+        if (lef <= tree[src << 1].rig) res = merge(res, query(src << 1, lef, rig));
+        if (rig >= tree[src << 1 | 1].lef) res = merge(res, query(src << 1 | 1, lef, rig));
+        return res;
+    }
+};
--- 通用/实用代码.cpp
+++ 通用/实用代码.cpp
@@ -1,16 +1,22 @@
-// debug 常用宏
+//debug 常用宏
 #define debug(x) cout << #x << " = " << x << endl
 
-// 本地文件读写
+//本地文件读写
 freopen("A.in", "r", stdin);
 freopen("A.out", "w", stdout);
 
-// builtin系列位运算
+//builtin 系列位运算
 __builtin_ffs(x); //最低位1是第几位（从1开始，不存在则0）
 __builtin_clz(x)/__builtin_clzll(x); //前导高0的个数
 __builtin_ctz(x)/__builtin_ctzll(x); //末尾低0的个数
 __builtin_popcount(x); //1的个数
 __builtin_parity(x); //1的个数的奇偶性
+
+//最高位 1 的位置（从0开始，注意x不能为0）
+__lg(x);
+
+//long double 用浮点函数后面加l
+sqrtl(x)/fabsl(x)/cosl(x);
 
 // 随机数生成器（C++11，返回unsigned_int）
 mt19937 mt(time(0));
