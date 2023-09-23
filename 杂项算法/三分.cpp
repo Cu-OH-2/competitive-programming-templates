708dc45af00a7d83564e7d9405e310e7178ecf40
Cu_OH_2 <850250678@qq.com>
2023-09-23 20:57:43 +0800

    更新三分

---
+++ 杂项算法/三分.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************
+* 时间复杂度：O(logn)
+* 说明：注意凹还是凸
+*******************************************************************/
+// 浮点数三分
+ld tes(ld lef, ld rig)
+{
+    if (fabs(lef - rig) < 1e-7) return lef;
+    ld midl = lef + (rig - lef) / 3;
+    ld midr = rig - (rig - lef) / 3;
+    ld resl = check(midl), resr = check(midr);
+    if (resl > resr) return tes(lef, midr);
+    else return tes(midl, rig);
+}
+
+// 整数三分 [l,r]
+ll tes(ll lef, ll rig)
+{
+    if (lef == rig) return lef;
+    ll midl = lef + (rig - lef) / 3;
+    ll midr = rig - (rig - lef) / 3;
+    ll resl = check(midl), resr = check(midr);
+    if (resl >= resr) return tes(lef, midr - 1);
+    else return tes(midl + 1, rig);
+}
