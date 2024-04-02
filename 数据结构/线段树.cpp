7acc02b395668b30e1b2d856024ba137c8de60df
Cu_OH_2 <850250678@qq.com>
2024-04-02 13:02:39 +0800

    修改线段树

--- 数据结构/线段树.cpp
+++ 数据结构/线段树.cpp
@@ -1,10 +1,6 @@
 /*******************************************************************
 * 时间复杂度：建立O(n)/询问O(logn)/修改O(logn)
-* 说明：
-* 1.维护区间性质，要求性质能由子区间性质得到。
-* 2.区间修改，区间查询。若仅单点修改则不需要标记。
-* 3.使线段树维护不同性质只需要改变上方函数和默认值。
-* 4.modify也要push。
+* 说明：维护区间和，支持区间加减
 *******************************************************************/
 struct SegTree
 {
@@ -15,19 +11,22 @@ struct SegTree
     };
     vector<Node> tree;
 
-    const ll VDEF = 0;
-    const ll TDEF = 0;
+    SegTree() {}
+    SegTree(int x) { tree.resize(x * 4 + 1); }
 
-    inline void update(int src) //由子节点及其标记更新父节点
+    // 由子节点及其标记更新父节点
+    void update(int src)
     {
         ll lw = tree[src << 1].rig - tree[src << 1].lef + 1;
         ll rw = tree[src << 1 | 1].rig - tree[src << 1 | 1].lef + 1;
         ll lv = tree[src << 1].val + tree[src << 1].tag * lw;
         ll rv = tree[src << 1 | 1].val + tree[src << 1 | 1].tag * rw;
-        tree[src].val = merge(lv, rv);
+        tree[src].val = lv + rv;
         return;
     }
-    inline void push(int src) //下传标记并消耗
+
+    // 下传标记并消耗
+    void pushdown(int src)
     {
         if (tree[src].lef < tree[src].rig)
         {
@@ -36,39 +35,26 @@ struct SegTree
         }
         ll wid = tree[src].rig - tree[src].lef + 1;
         tree[src].val += tree[src].tag * wid;
-        tree[src].tag = TDEF;
+        tree[src].tag = 0;
         return;
     }
-    inline void mark(int src, ll val) //更新标记
-    {
-        tree[src].tag += val;
-        return;
-    }
-    inline ll merge(ll x, ll y) //合并两个查询结果
-    {
-        return x + y;
-    }
 
-    SegTree() {}
-    SegTree(int x) { init(x); }
-    void init(int x) { tree.resize(x * 4 + 1); }
-
+    /*
     void build(int src, int lef, int rig, ll arr[])
     {
-        tree[src] = { lef, rig, VDEF, TDEF };
-        if (lef == rig) tree[src].val = arr[lef];
-        else
-        {
-            int mid = lef + (rig - lef) / 2;
-            build(src << 1, lef, mid, arr);
-            build(src << 1 | 1, mid + 1, rig, arr);
-            update(src);
-        }
+        tree[src] = { lef, rig, arr[lef], 0 };
+        if (lef == rig) return;
+        int mid = lef + (rig - lef) / 2;
+        build(src << 1, lef, mid, arr);
+        build(src << 1 | 1, mid + 1, rig, arr);
+        update(src);
         return;
     }
+    */
+
     void build(int src, int lef, int rig)
     {
-        tree[src] = { lef, rig, VDEF, TDEF };
+        tree[src] = { lef, rig, 0, 0 };
         if (lef == rig) return;
         int mid = lef + (rig - lef) / 2;
         build(src << 1, lef, mid);
@@ -76,9 +62,81 @@ struct SegTree
         update(src);
         return;
     }
+
+    void modify(int src, int lef, int rig, ll val)
+    {
+        if (lef <= tree[src].lef && tree[src].rig <= rig)
+        {
+            tree[src].tag += val;
+            return;
+        }
+        pushdown(src);
+        if (lef <= tree[src << 1].rig) modify(src << 1, lef, rig, val);
+        if (rig >= tree[src << 1 | 1].lef) modify(src << 1 | 1, lef, rig, val);
+        update(src);
+        return;
+    }
+
+    ll query(int src, int lef, int rig)
+    {
+        pushdown(src);
+        if (lef <= tree[src].lef && tree[src].rig <= rig) return tree[src].val;
+        ll res = 0;
+        if (lef <= tree[src << 1].rig) res += query(src << 1, lef, rig);
+        if (rig >= tree[src << 1 | 1].lef) res += query(src << 1 | 1, lef, rig);
+        return res;
+    }
+};
+
+/*******************************************************************
+* 时间复杂度：建立O(n)/询问O(logn)/修改O(logn)
+* 说明：维护区间和，支持单点修改
+*******************************************************************/
+struct SegTree
+{
+    struct Node
+    {
+        int lef, rig;
+        int val;
+    };
+    vector<Node> tree;
+
+    SegTree() {}
+    SegTree(int x) { tree.resize(x * 4 + 1); }
+
+    // 由子节点及其标记更新父节点
+    void update(int src)
+    {
+        tree[src].val = tree[src << 1].val + tree[src << 1 | 1].val;
+        return;
+    }
+
+    /*
+    void build(int src, int lef, int rig, ll arr[])
+    {
+        tree[src] = { lef, rig, arr[i] };
+        if (lef == rig) return;
+        int mid = lef + (rig - lef) / 2;
+        build(src << 1, lef, mid, arr);
+        build(src << 1 | 1, mid + 1, rig, arr);
+        update(src);
+        return;
+    }
+    */
+
+    void build(int src, int lef, int rig)
+    {
+        tree[src] = { lef, rig, 0 };
+        if (lef == rig) return;
+        int mid = lef + (rig - lef) / 2;
+        build(src << 1, lef, mid);
+        build(src << 1 | 1, mid + 1, rig);
+        update(src);
+        return;
+    }
+
     void assign(int src, int pos, ll val)
     {
-        push(src);
         if (tree[src].lef == tree[src].rig)
         {
             tree[src].val = val;
@@ -89,26 +147,108 @@ struct SegTree
         update(src);
         return;
     }
+
+    ll query(int src, int lef, int rig)
+    {
+        if (lef <= tree[src].lef && tree[src].rig <= rig) return tree[src].val;
+        ll res = 0;
+        if (lef <= tree[src << 1].rig) res += query(src << 1, lef, rig);
+        if (rig >= tree[src << 1 | 1].lef) res += query(src << 1 | 1, lef, rig);
+        return res;
+    }
+};
+
+/*******************************************************************
+* 时间复杂度：建立O(n)/询问O(logn)/修改O(logn)
+* 说明：维护最大值，支持区间加减/二分查询第一个大于等于x的数
+*******************************************************************/
+struct SegTree
+{
+    struct Node
+    {
+        int lef, rig;
+        ll val, tag;
+    };
+    vector<Node> tree;
+
+    SegTree() {}
+    SegTree(int x) { tree.resize(x * 4 + 1); }
+
+    // 由子节点及其标记更新父节点
+    void update(int src)
+    {
+        ll lv = tree[src << 1].val + tree[src << 1].tag;
+        ll rv = tree[src << 1 | 1].val + tree[src << 1 | 1].tag;
+        tree[src].val = max(lv, rv);
+        return;
+    }
+
+    // 下传标记并消耗
+    void pushdown(int src)
+    {
+        if (tree[src].lef < tree[src].rig)
+        {
+            tree[src << 1].tag += tree[src].tag;
+            tree[src << 1 | 1].tag += tree[src].tag;
+        }
+        tree[src].val += tree[src].tag;
+        tree[src].tag = 0;
+        return;
+    }
+
+    /*
+    void build(int src, int lef, int rig, ll arr[])
+    {
+        tree[src] = { lef, rig, arr[lef], 0 };
+        if (lef == rig) return;
+        int mid = lef + (rig - lef) / 2;
+        build(src << 1, lef, mid, arr);
+        build(src << 1 | 1, mid + 1, rig, arr);
+        update(src);
+        return;
+    }
+    */
+
+    void build(int src, int lef, int rig)
+    {
+        tree[src] = { lef, rig, 0, 0 };
+        if (lef == rig) return;
+        int mid = lef + (rig - lef) / 2;
+        build(src << 1, lef, mid);
+        build(src << 1 | 1, mid + 1, rig);
+        update(src);
+        return;
+    }
+
     void modify(int src, int lef, int rig, ll val)
     {
         if (lef <= tree[src].lef && tree[src].rig <= rig)
         {
-            mark(src, val);
+            tree[src].tag += val;
             return;
         }
-        push(src);
+        pushdown(src);
         if (lef <= tree[src << 1].rig) modify(src << 1, lef, rig, val);
         if (rig >= tree[src << 1 | 1].lef) modify(src << 1 | 1, lef, rig, val);
         update(src);
         return;
     }
+
     ll query(int src, int lef, int rig)
     {
-        push(src);
+        pushdown(src);
         if (lef <= tree[src].lef && tree[src].rig <= rig) return tree[src].val;
-        ll res = VDEF;
-        if (lef <= tree[src << 1].rig) res = merge(res, query(src << 1, lef, rig));
-        if (rig >= tree[src << 1 | 1].lef) res = merge(res, query(src << 1 | 1, lef, rig));
+        ll res = 0;
+        if (lef <= tree[src << 1].rig) res = max(res, query(src << 1, lef, rig));
+        if (rig >= tree[src << 1 | 1].lef) res = max(res, query(src << 1 | 1, lef, rig));
         return res;
+    }
+
+    int bis(int src, ll tar)
+    {
+        if(tree[src].val + tree[src].tag < tar) return tree[src].rig + 1;
+        if(tree[src].lef == tree[src].rig) return tree[src].lef;
+        if(tree[src << 1].val + tree[src << 1].tag >= tar) return bis(src << 1, tar);
+        else return bis(src << 1 | 1, tar);
     }
 };
